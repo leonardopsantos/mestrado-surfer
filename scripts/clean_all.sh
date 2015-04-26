6cd8c194efdfe2558305138606ec3d48df4ca9c3
Leonardo Pereira Santos <leonardo.santos@restinga.ifrs.edu.br>
2015-04-26 00:41:52 -0300

    Fixed clean_all script, now you can call it anywhere.

--- scripts/clean_all.sh
+++ scripts/clean_all.sh
@@ -1,34 +1,9 @@
-rm benches/*/*_vhdl.prj
-rm benches/*/*.ngc
-rm benches/*/*.ngd
-rm benches/*/*.ncd
-rm benches/*/*.xpi
-rm benches/*/*.xml
-rm benches/*/*.lst
-rm benches/*/*.twr
-rm benches/*/*.twx
-rm benches/*/*.log
-rm benches/*/*.bgn
-rm benches/*/*.bit
-rm benches/*/*.drc
-rm benches/*/*.ngm
-rm benches/*/*.xwbt
-rm -rf benches/*/_xmsgs
-rm -rf benches/*/xst
-rm -rf benches/*/xln_*
-rm -rf benches/*/*.bld
-rm -rf benches/*/*.map.map
-rm -rf benches/*/*.map.*
-rm -rf benches/*/*.pad
-rm -rf benches/*/*.par
-rm -rf benches/*/*.srp
-rm -rf benches/*/*.pcf
-rm -rf benches/*/*.ptwx
-rm -rf benches/*/*.unroutes
-rm -rf benches/*/*.ngc_xst.*
-rm -rf benches/*/*_ngdbuild*
-rm -rf benches/*/*.txt
-rm -rf benches/*/*.csv
-rm -rf benches/*/xlnx_*
-rm -rf benches/*/*.lso
-rm -rf benches/*/*.xrpt
+#!/bin/bash
+
+for e in ngc ngd ncd xpi xml lst twr twx log bgn bit drc ngm xwbt bld map.* xrpt; do
+	find -iname "*.$e" | grep -v "ipcore_dir" | xargs rm -f
+done
+
+for e in xst bld map.map map.* pad par srp pcf ptwx unroutes ngc_xst.* txt csv lso xrpt xlnx_* *_vhdl.prj xln_* _ngdbuild* *_xmsgs; do
+	find -iname "*$e" | grep -v "ipcore_dir" | xargs rm -fr
+done
