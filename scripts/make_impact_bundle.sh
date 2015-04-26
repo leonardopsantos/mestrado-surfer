3300c3ed97f1556c9046095512b5a649b3f61337
Leonardo Pereira Santos <leonardo.santos@restinga.ifrs.edu.br>
2015-04-26 00:42:25 -0300

    Fixed bugs. Now it only makes bundles with benchmarks that have bitfiles.

--- scripts/make_impact_bundle.sh
+++ scripts/make_impact_bundle.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+SCRIPTS=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+IMPACT=$SCRIPTS/../base_projects/base_impact_project
+
 if [ ! -x impact_bundle ]; then
 	mkdir impact_bundle
 fi
 
-##for bench in $(ls benches/); do
-for bench in alu4 pdc; do
+BNCHS=$(find benches -iname "*.bit" | cut -d'/' -f 2)
+
+for bench in $BNCHS; do
 	if [ ! -x impact_bundle/$bench ]; then
 		mkdir impact_bundle/$bench
 	fi
 
 	cp -af benches/$bench/$bench.bit impact_bundle/$bench
 
-	cp -af impact_projs/$bench/program.cmd impact_bundle/$bench
-	cp -af impact_projs/$bench/project.ipf impact_bundle/$bench
+	
+	cat $IMPACT/program.cmd | sed s/BITFILENAME/$bench.bit/ > impact_bundle/$bench/program.cmd
+	cp -af $IMPACT/project.ipf impact_bundle/$bench/
 done
 
-	cp -af ../src/capture_and_check/capture impact_bundle
-	cp -af program_and_capture.sh impact_bundle
+	cp -af $SCRIPTS/../src/capture_and_check/capture impact_bundle
+	cp -af $SCRIPTS/program_and_capture.sh impact_bundle
+	
