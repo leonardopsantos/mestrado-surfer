010d32861bb65294f81f6f526431d71f7b9d0344
Leonardo Pereira Santos <leonardo.santos@restinga.ifrs.edu.br>
2015-07-13 20:30:16 -0300

    Renamed selected_nets to interesting_nets in buildFtSelectiveCirc.

--- src/surfer/ftSelectiveXilinx.cpp
+++ src/surfer/ftSelectiveXilinx.cpp
@@ -14,13 +14,13 @@ using namespace std;
 ftSelectiveXilinx::ftSelectiveXilinx(int maxsize)
 {
 	this->maxsize = maxsize;
-//	this->selected_nets = vector<std::pair<Net*, double> >(this->maxsize);
+	this->selected_nets = vector<Net*>(this->maxsize);
 }
 
 int ftSelectiveXilinx::buildFtSelectiveCirc(Circuit& synth_circ,
 		Circuit& circ_cpy0, Circuit& circ_cpy1)
 {
-	vector<std::pair<Net*, double> > selected_nets(this->maxsize);
+	vector<std::pair<Net*, double> > interesting_nets(this->maxsize);
 	double mean, sigma;
 
 	int i;
@@ -37,7 +37,7 @@ int ftSelectiveXilinx::buildFtSelectiveCirc(Circuit& synth_circ,
 	cout << "---------------------" << endl;
 
 	vector<std::pair<Net*, double> >::iterator sel_it;
-	for(sel_it = selected_nets.begin(); sel_it < selected_nets.end(); sel_it++) {
+	for(sel_it = interesting_nets.begin(); sel_it < interesting_nets.end(); sel_it++) {
 		sel_it->first = circ_cpy0.nets[0];
 		sel_it->second = 999999;
 	}
@@ -140,7 +140,7 @@ int ftSelectiveXilinx::buildFtSelectiveCirc(Circuit& synth_circ,
 
 		sigma = (sigma/(n->outputs.size()+1));
 
-		for(sel_it = selected_nets.begin(); sel_it < selected_nets.end(); sel_it++) {
+		for(sel_it = interesting_nets.begin(); sel_it < interesting_nets.end(); sel_it++) {
 			if( sel_it->second > sigma ) {
 				sel_it->first = n;
 				sel_it->second = sigma;
@@ -153,7 +153,7 @@ int ftSelectiveXilinx::buildFtSelectiveCirc(Circuit& synth_circ,
 //			<< maxLoc.first << "," << maxLoc.second << ")\n";
 //
 //	cout << "Selected Nets:\n";
-//	for(sel_it = selected_nets.begin(); sel_it < selected_nets.end(); sel_it++) {
+//	for(sel_it = interesting_nets.begin(); sel_it < interesting_nets.end(); sel_it++) {
 //		cout << "    " << sel_it->first->name << " sigma : " << sel_it->second << " : ";
 //		for(comp_it = sel_it->first->outputs.begin(); comp_it < sel_it->first->outputs.end(); comp_it++) {
 //			comp = *comp_it;
@@ -163,7 +163,7 @@ int ftSelectiveXilinx::buildFtSelectiveCirc(Circuit& synth_circ,
 //	}
 //	cout << "\n";
 
-//	for(sel_it = selected_nets.begin(); sel_it < selected_nets.end(); sel_it++) {
+//	for(sel_it = interesting_nets.begin(); sel_it < interesting_nets.end(); sel_it++) {
 //		sel_it->first = circ_cpy0.nets[0];
 //		sel_it->second = 999999;
 //	}
@@ -262,7 +262,7 @@ int ftSelectiveXilinx::buildFtSelectiveCirc(Circuit& synth_circ,
 
 		sigma = (sigma/(n->outputs.size()+1));
 
-		for(sel_it = selected_nets.begin(); sel_it < selected_nets.end(); sel_it++) {
+		for(sel_it = interesting_nets.begin(); sel_it < interesting_nets.end(); sel_it++) {
 			if( sel_it->second > sigma ) {
 				sel_it->first = n;
 				sel_it->second = sigma;
@@ -271,11 +271,15 @@ int ftSelectiveXilinx::buildFtSelectiveCirc(Circuit& synth_circ,
 		}
 	}
 
+	for(i=0; i < interesting_nets.size(); i++) {
+		this->selected_nets[i] = interesting_nets[i].first;
+	}
+
 	cout << circ_cpy1.name <<" min LOC = (" << minLoc.first << "," << minLoc.second << "), max LOC = ("
 			<< maxLoc.first << "," << maxLoc.second << ")\n";
 
 	cout << "Selected Nets:\n";
-	for(sel_it = selected_nets.begin(); sel_it < selected_nets.end(); sel_it++) {
+	for(sel_it = interesting_nets.begin(); sel_it < interesting_nets.end(); sel_it++) {
 		cout << "    " << sel_it->first->name << " sigma : " << sel_it->second << " : ";
 		for(comp_it = sel_it->first->outputs.begin(); comp_it < sel_it->first->outputs.end(); comp_it++) {
 			comp = *comp_it;
@@ -339,7 +343,7 @@ int ftSelectiveXilinx::buildFtSelectiveCirc(Circuit& synth_circ,
 		dist[n->input->locX-minLoc.first][n->input->locY-minLoc.second] = '1';
 	}
 
-	for(sel_it = selected_nets.begin(); sel_it < selected_nets.end(); sel_it++) {
+	for(sel_it = interesting_nets.begin(); sel_it < interesting_nets.end(); sel_it++) {
 		dist[sel_it->first->input->locX-minLoc.first][sel_it->first->input->locY-minLoc.second] = '*';
 	}
 
