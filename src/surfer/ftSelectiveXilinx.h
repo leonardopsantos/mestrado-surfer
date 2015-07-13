e8543ff026672d868c79aa4909853902f0db44a1
Leonardo Pereira Santos <leonardo.santos@restinga.ifrs.edu.br>
2015-07-13 20:25:39 -0300

    Fixed bug in ftSelectiveXilinx.h

--- src/surfer/ftSelectiveXilinx.h
+++ src/surfer/ftSelectiveXilinx.h
@@ -3,15 +3,20 @@
     \author Leonardo Pereira Santos <leonardo.santos@restinga.ifrs.edu.br>
 */
 
+#ifndef _FTSELECTIVEXILINX_H_
+#define _FTSELECTIVEXILINX_H_
+
+
 #include "circuit.h"
 
 class ftSelectiveXilinx {
 
 public:
+	int maxsize;	//!< Number of Nets to be selected to comparison
+	vector<Net*> selected_nets;
+
 	ftSelectiveXilinx(int maxsize);
 	int buildFtSelectiveCirc(Circuit &synth_circ, Circuit &circ_cpy0, Circuit &circ_cpy1);
-
-protected:
-	int maxsize;	//!< Number of Nets to be selected to comparison
-//	vector<std::pair<Net*, double> > selected_nets;
 };
+
+#endif // _FTSELECTIVEXILINX_H_
